d34ea7efd497ee3e92241356d6c49896d9bdc6e0
sofiiaboichun <sofiaboychun14@gmail.com>
2021-06-02 00:16:01 +0300

    dialog window

--- pdrApp/app/src/main/java/space/boichun/pdrapp/Level1.java
+++ pdrApp/app/src/main/java/space/boichun/pdrapp/Level1.java
@@ -1,6 +1,9 @@
 package space.boichun.pdrapp;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,6 +15,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,36 @@ public class Level1 extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.previewdialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+
+        TextView btnclose = (TextView)dialog.findViewById(R.id.btnclose);
+        btnclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent = new Intent(Level1.this,TestLevels.class);
+                    startActivity(intent);finish();
+                }catch (Exception e) {
+                }
+                dialog.dismiss();
+            }
+        });
+
+        Button btncont = (Button) dialog.findViewById(R.id.btncont);
+        btncont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.show();
 
 
     }
